d3031d1b09b0747762fbe2a26df7ab6bb9ab6e2e
Dani Bachs <danibachsl@protonmail.com>
2023-06-27 12:25:44 +0200

    setting providers

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -81,6 +81,36 @@ const Nav = () => {
                             alt="profile"
                             onClick={() => setToggleDropdown((prev) => !prev)}
                         />
+
+                        {toggleDropdown && (
+                            <div className='dropdown'>
+                                <Link
+                                    href="/profile"
+                                    className='dropdown_link'
+                                    onClick={() => setToggleDropdown(false)}
+                                >
+                                    My Profile
+                                </Link>
+                                <Link
+                                    href="/create-prompt"
+                                    className='dropdown_link'
+                                    onClick={() => setToggleDropdown(false)}
+                                >
+                                    Create Prompt
+                                </Link>
+
+                                <button
+                                    type='button'
+                                    onClick={() => {
+                                        setToggleDropdown(false);
+                                        signOut();
+                                    }}
+                                    className="mt-5 w-full black_btn"
+                                >
+                                    Sign Out
+                                </button>
+                            </div>
+                        )}
                     </div>
                 ) : (
                     <>
