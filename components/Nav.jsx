5178b0a7f88fa59e2b16c805f2f9d9cd8e786fb0
Dani Bachs <danibachsl@protonmail.com>
2023-06-27 11:36:40 +0200

    developing Nav bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
         <Link href="/" className='flex gap-2 flex-center'>
@@ -19,6 +21,34 @@ const Nav = () => {
             <p className="logo_text">Promptopia</p>
         </Link>
 
+        {/* Desktop Navigation */}
+        <div className='sm:flex hidden'>
+            {isUserLoggedIn ? (
+                <div className='flex gap-3 md:gap-5'>
+                    <Link href='/create-prompt' className='black_btn'>
+                        Create Post
+                    </Link>
+
+                    <button type='button' onClick={signOut} className="outline_btn">
+                        Sign Out
+                    </button>
+
+                    <Link href="/profile">
+                        <Image 
+                            src="/assets/images/logo.svg"
+                            width={37}
+                            height={37}
+                            className="rounded-full"
+                            alt="profile"
+                        />
+                    </Link>
+                </div>
+            ) : (
+                <>
+
+                </>
+            )}
+        </div>
     </nav>
   )
 }
