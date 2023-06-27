a2f7fa1b0a9a36898b3156fbbc4c32724dc9fb07
Dani Bachs <danibachsl@protonmail.com>
2023-06-27 11:46:25 +0200

    sign in & sign out buttons

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,18 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 const Nav = () => {
   const isUserLoggedIn = true;
 
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+        const response = await getProviders();
+
+        setProviders(response);
+    }
+
+    setProviders();
+  }, [])
+
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
         <Link href="/" className='flex gap-2 flex-center'>
@@ -45,7 +57,13 @@ const Nav = () => {
                 </div>
             ) : (
                 <>
-
+                    {providers && 
+                        Object.values(providers).map(() => (
+                            <button type="button" key={providers.name} onClick={() => signIn(provider.id)} className="black_btn">
+                                Sign In
+                            </button>
+                        ))
+                    }
                 </>
             )}
         </div>
